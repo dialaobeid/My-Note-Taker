30f6eb38be67f556fc93d5e8829821da766270a0
dobeid <dialajo9@gmail.com>
2024-03-14 16:47:27 -0700

    updated assets folder by adding an images folder with screenshot, edited any changes needed

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -136,7 +136,7 @@ const renderNoteList = async (notes) => {
 
   let noteListItems = [];
 
-  // Returns HTML element with or without a delete button
+  // Returns HTML element with or w/o delete button
   const createLi = (text, delBtn = true) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item');
@@ -181,7 +181,7 @@ const renderNoteList = async (notes) => {
   }
 };
 
-// Gets notes from the db and renders them to the sidebar
+// Gets notes from db & renders them to sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
 if (window.location.pathname === '/notes') {
